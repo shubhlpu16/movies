87ed1da7e73f7c345a1d38a63233c14dc1e5e0ac
Shubham Saxena <shubh.lpu16@gmail.com>
2024-07-21 20:32:28 +0530

    fix

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((e) => {
-  console.log(e);
-});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,101 @@
-require('dotenv').config();
-const express = require('express');
-require('./db/mongoose.js');
-cors = require('cors');
-const userRouter = require('./routers/user.js');
-const movieRouter = require('./routers/movie.js');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const webpush = require("web-push");
 
-const port = process.env.PORT;
-
-
-app.use(express.static("public")) 
 app.use(cors());
 app.use(express.json());
-app.use('/api',userRouter);
-app.use('/api',movieRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Movie API');
+const subDatabase = [];
 
-})
+// Example route to demonstrate middleware usage
+app.get("/", (req, res) => {
+  res.send("This is socket for shoowflix");
+});
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)});
+io.on("connection", (socket) => {
+  console.log("Connected to server");
+
+  socket.on("join", async (userId) => {
+    console.log(`User ${userId} joined`);
+    // Additional logic for handling user sessions
+  });
+  socket.on("disconnect", (userId) => {
+    console.log(`${userId} Disconnected from server`);
+  });
+
+  socket.on("sendNotification", ({ validUsers, commentId }) => {
+    validUsers.map((user) =>
+      io.to(user.id).emit("notification", {
+        type: "mention",
+        message: `You were mentioned in a comment by @${user.userName}`,
+        commentId: commentId,
+        createdAt: new Date(),
+      })
+    );
+  });
+});
+
+const apiKeys = {
+  pubKey:
+    "BNCR_N209rz2iw20xIKTK5KZHr8Dd8uMZJaBlAXuCajqAy5lFLUVzraMIWozdMbiTqRkw6LglstMkCpYQyPmNa8",
+  privKey: "HXZ2m2tDbU7XOV0b6CX0zHDFds11PBADBNx9Puf05rU",
+};
+
+webpush.setVapidDetails(
+  "mailto:shubh.lpu16@gamil.com",
+  apiKeys.pubKey,
+  apiKeys.privKey
+);
+
+app.post("/api/save-subscription", (req, res) => {
+  subDatabase.push(req.body);
+  res.status(200).json({ status: "Success", message: "Subscription saved!" });
+});
+// app.post("/api/notify", async (req, res) => {
+//   const { validUsers, commentId } = req.body;
+//   if (!io) {
+//     return res.status(500).send("Socket.io is not initialized");
+//   }
+
+//   try {
+//     // Use Promise.all to wait for all notifications to be sent
+//     await Promise.all();
+//   } catch (error) {
+//     console.error("Failed to send notifications:", error);
+//     res.status(500).send("Failed to send notifications");
+//   }
+// });
+
+app.post("api/send-notification", (req, res) => {
+  if (subDatabase.length > 0) {
+    webpush
+      .sendNotification(subDatabase[0], req.body.message)
+      .then(() => {
+        res.status(200).send({
+          status: "Success",
+          message: "Message sent to push service",
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ status: "Error", message: error.toString() });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ status: "Error", message: "No subscriptions found" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = app;
